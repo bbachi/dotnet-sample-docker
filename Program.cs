472d2855c53fd2feaac45bff4bd72b58dd8358e4
bbachi <bhargavbachina@yahoo.com>
2020-06-09 21:08:39 -0500

    addedd manifest yml

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace dotnet_sample_docker
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:5000");
                 });
     }
 }
